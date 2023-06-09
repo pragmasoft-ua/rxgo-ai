78b04a63b5e00d72802bccc597276ef8a5b7ed9e
Dmytro Zemnytskyi <pragmasoft@gmail.com>
2023-06-09 17:05:41 +0300

    small enhancemens, updated default prompt, better error handling

--- src/main/java/com/rxgo/CompletionCommand.java
+++ src/main/java/com/rxgo/CompletionCommand.java
@@ -4,19 +4,19 @@ import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.theokanning.openai.OpenAiHttpException;
 import com.theokanning.openai.completion.chat.ChatCompletionChoice;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatCompletionResult;
@@ -35,33 +35,42 @@ public class CompletionCommand implements Runnable {
   static final String DEFAULT_PROMPT = """
       user: Create SEO friendly title tags for "%drugname% coupon" to be used on RxGo.
       The two main search terms the title tag should always start with either
-      "%drugname% coupons" or "%drugname% discounts".
-      Exclude the words "code", "guarantee" and "best".
+      "%drugname% coupons" or "%drugname% discounts" but not "%drugname% coupons and discounts" or
+      "%drugname% discounts and coupons"
+      Exclude the words "code", "guarantee" "best", "healthcare" and "therapy".
       Provide variants
-      assistant: %drugname% Discounts and Coupons from RxGo
+      assistant: %drugname% Coupons from RxGo
       %drugname% Coupons | Save Money on Your Prescription - RxGo.com
-      %drugname% Discounts and Coupons | RxGo
-      %drugname% Discounts and Coupons | shop smart with RxGo
+      %drugname% Coupons | RxGo prescription discounts
+      %drugname% Discounts | shop smart with RxGo's prescription coupons
+      RxGo: Your Prescription Savings Hub for %drugname% Coupons
+      RxGo: Your Destination for %drugname% Discounts
+      %drugname% Coupons | RxGo - Your Prescription Savings Partner
+      RxGo: %drugname% Coupons - Save with our discounts
+      %drugname% Coupons Use RxGo for Cost-Effective Medication Discounts
+      RxGo: Your Prescription Savings Destination for %drugname% Discounts
       """;
 
   static final String DEFAULT_MODEL = "gpt-3.5-turbo";
-  static final int MAX_TOKENS = 4000;
-  static final int MAX_RESULTS = 100;
+  static final String DEFAULT_TEMP = "0.5";
+  static final int CONTEXT_SIZE = 4000;
+  static final int ENOUGH_RESULTS = 100;
+  static final int MAX_ITERATIONS = 30;
+  static final int MAX_NORESULT_ITERATIONS = 3;
+  static final Pattern STOP_WORDS_PATTERN = Pattern.compile(
+      ".*\\b(code|best|guarantee|healthcare|therapy)\\b.*",
+      Pattern.CASE_INSENSITIVE);
 
   final OpenAiService openAi;
-  final PrintStream output;
-  final Set<String> result;
 
   /**
    * @param openAi
    */
   public CompletionCommand(OpenAiService openAi) {
     this.openAi = openAi;
-    this.output = System.out;
-    this.result = new HashSet<>(MAX_RESULTS);
   }
 
-  @Parameters(paramLabel = "temperature", defaultValue = "0.6", description = "temperature")
+  @Parameters(paramLabel = "temperature", defaultValue = DEFAULT_TEMP, description = "temperature")
   Double temperature;
 
   @Parameters(paramLabel = "model", defaultValue = DEFAULT_MODEL, description = "model name")
@@ -69,62 +78,91 @@ public class CompletionCommand implements Runnable {
 
   @Override
   public void run() {
-    long iterationTokens = MAX_TOKENS / 4;
-    long totalTokens = 0;
-    List<ChatMessage> originalPrompt = PromptParser.parsePrompts(this.readPromptFromStdinOrDefault());
-    List<ChatMessage> prompt = List.copyOf(originalPrompt);
-    for (int i = 0, converged = 0; i < MAX_RESULTS && converged < 3; i++) {
-      ChatCompletionRequest request = ChatCompletionRequest
-          .builder()
-          .temperature(this.temperature)
-          .model(this.model)
-          .messages(prompt)
-          .maxTokens((int) (MAX_TOKENS - iterationTokens))
-          .build();
-      LOG.debug("\nPrompt\n{}", request);
-      ChatCompletionResult response = this.openAi.createChatCompletion(request);
-      var choices = response.getChoices()
-          .stream()
-          .map(ChatCompletionChoice::getMessage)
-          .filter(c -> c.getRole().equals(ChatMessageRole.ASSISTANT.value()))
-          .map(ChatMessage::getContent)
-          .flatMap(String::lines)
-          .map(String::strip)
-          .filter(this::validate)
-          .collect(Collectors.toSet());
-      iterationTokens = response.getUsage().getTotalTokens();
-      totalTokens += iterationTokens;
-      choices.removeAll(this.result);
-      LOG.debug("\nCollected {} choices\n{}", choices.size(), choices);
 
-      if (!choices.isEmpty()) {
-        this.result.addAll(choices);
-        converged = 0;
-      } else {
-        converged++;
-      }
+    final List<ChatMessage> prompt = PromptParser.parsePrompts(this.readPromptFromStdinOrDefault());
+    final int firstResponseIndex = prompt.size();
+    final Set<String> results = new HashSet<>(ENOUGH_RESULTS);
+    long totalTokensSpent = 0;
+    int noResultsIterations = 0;
 
-      LOG.debug("\nTotal results after {} iterations collected: {}", i + 1, this.result.size());
-      if (this.result.size() >= MAX_RESULTS) {
-        break;
-      }
+    for (int i = 0; i < MAX_ITERATIONS && noResultsIterations < MAX_NORESULT_ITERATIONS; i++) {
+      final ChatCompletionRequest request = prepareRequest(prompt);
+      LOG.trace("\nPrompt\n{}", request);
+      try {
+        ChatCompletionResult response = this.openAi.createChatCompletion(request);
+        var choices = responseToLines(response);
+        final long iterationTokensSpent = response.getUsage().getTotalTokens();
+        totalTokensSpent += iterationTokensSpent;
+        // We need to remove the choices that we have already seen
+        choices.removeAll(results);
+        LOG.trace("\nCollected {} choices\n{}", choices.size(), choices);
 
-      if (!choices.isEmpty()) {
-        String joinedResponses = this.result.stream().collect(Collectors.joining("\n"));
-        prompt = new ArrayList<>(originalPrompt);
-        prompt.add(new ChatMessage(ChatMessageRole.ASSISTANT.value(), joinedResponses));
+        if (!choices.isEmpty()) {
+          results.addAll(choices);
+          noResultsIterations = 0;
+        } else {
+          noResultsIterations++;
+        }
+
+        LOG.debug("\nTotal results after {} iterations collected: {}", i + 1, results.size());
+        if (results.size() >= ENOUGH_RESULTS) {
+          break;
+        }
+
+        // feed the results back into the prompt
+        if (!choices.isEmpty()) {
+          String joinedResponses = choices.stream().collect(Collectors.joining("\n"));
+          prompt.add(new ChatMessage(ChatMessageRole.ASSISTANT.value(), joinedResponses));
+          if (iterationTokensSpent > CONTEXT_SIZE / 2) {
+            // we need to remove the oldest results
+            prompt.remove(firstResponseIndex);
+          }
+        }
+      } catch (OpenAiHttpException e) {
+        LOG.error("Error calling OpenAI API endpoint", e);
+        noResultsIterations++;
       }
     }
-    LOG.info("Total tokens spent: {} total results collected: {}", totalTokens, this.result.size());
-    try (var fileToSave = new PrintWriter(new FileWriter("titles.csv", false))) {
-      this.result.forEach(fileToSave::println);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+
+    LOG.debug(
+        "Total tokens spent: {} total results collected: {}",
+        totalTokensSpent,
+        results.size());
+
+    saveResultsToFile(results);
 
   }
 
-  private Readable readPromptFromStdinOrDefault() {
+  ChatCompletionRequest prepareRequest(List<ChatMessage> prompt) {
+    return ChatCompletionRequest
+        .builder()
+        .temperature(this.temperature)
+        .model(this.model)
+        .messages(prompt)
+        .build();
+  }
+
+  Set<String> responseToLines(ChatCompletionResult response) {
+    return response.getChoices()
+        .stream()
+        .map(ChatCompletionChoice::getMessage)
+        .filter(c -> c.getRole().equals(ChatMessageRole.ASSISTANT.value()))
+        .map(ChatMessage::getContent)
+        .flatMap(String::lines)
+        .map(String::strip)
+        .filter(this::validateLine)
+        .collect(Collectors.toSet());
+  }
+
+  void saveResultsToFile(Iterable<String> results) {
+    try (var fileToSave = new PrintWriter(new FileWriter("titles.csv", false))) {
+      results.forEach(fileToSave::println);
+    } catch (IOException e) {
+      LOG.error("Failed to save results", e);
+    }
+  }
+
+  Readable readPromptFromStdinOrDefault() {
     if (System.console() != null) {
       // We have interactive console, use default prompt
       return new StringReader(DEFAULT_PROMPT);
@@ -135,9 +173,10 @@ public class CompletionCommand implements Runnable {
     }
   }
 
-  private boolean validate(String response) {
-    // TODO implement validation logic
-    return true;
+  boolean validateLine(String response) {
+    // use regexp to verify that response does not contain one of stop words
+    return response.contains("%drugname%") &&
+        !STOP_WORDS_PATTERN.matcher(response).matches();
   }
 
 }
